719e23448a57235a856b1f08e92efaf06fe5f71d
bosk <bovlovsk@yandex.com>
2017-09-16 22:17:11 +0800

    fix main

--- anothervote_main.go
+++ anothervote_main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"./exec"
-	//_ "github.com/henrylee2cn/pholcus_lib" // 此为公开维护的spider规则库
-	// _ "github.com/henrylee2cn/pholcus_lib_pte" // 同样你也可以自由添加自己的规则库
-	_ "../pholcusrules"
+	"github.com/bovlov/anothervote/exec"
+	//_ "github.com/bovlov/anothervote_lib" // 此为公开维护的spider规则库
+	// _ "github.com/bovlov/anothervote_lib_pte" // 同样你也可以自由添加自己的规则库
+	_ "./otherrule"
 )
 
 func main() {
